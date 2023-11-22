e1f843f7b7b2cb907f133a16c3509458e167b246
Kristina Popova <kristina_14_09@vk.com>
2023-11-22 10:30:10 +0300

    first task

---
+++ ЛАБ3/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace ЛАБ3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(TextBox1.Text, out double A))
+            {
+                MessageBox.Show("Поле А только число!");
+            }
+            if (!double.TryParse(textbox2.Text, out double B))
+            {
+                MessageBox.Show("Поле B только число!");
+            }
+            if (!double.TryParse(textbox3.Text, out double C))
+            {
+                MessageBox.Show("Поле C только число!");
+            }
+            A = B;
+            B = C;
+            C = A;
+            TextBox4.Text += $"{A}{B}{C}\n";
+
+        }
+    }
+}
